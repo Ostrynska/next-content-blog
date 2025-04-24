289335e664af1ec4772d409112f6c2dea4ae3bef
Ostrynska <kateriinag@gmail.com>
2025-04-24 20:58:55 +0300

    create project

---
+++ src/theme/AppTheme.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import * as React from 'react';
+import type { ThemeOptions } from '@mui/material/styles';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+import { inputsCustomizations } from './customizations/inputs';
+import { dataDisplayCustomizations } from './customizations/dataDisplay';
+import { feedbackCustomizations } from './customizations/feedback';
+import { navigationCustomizations } from './customizations/navigation';
+import { surfacesCustomizations } from './customizations/surfaces';
+import { colorSchemes, shadows, shape } from './themePrimitives';
+
+interface AppThemeProps {
+  children: React.ReactNode;
+  themeComponents?: ThemeOptions['components'];
+}
+
+export default function AppTheme(props: AppThemeProps) {
+  const { children, themeComponents } = props;
+  const theme = React.useMemo(() => {
+    return createTheme({
+      cssVariables: {
+        colorSchemeSelector: 'data-mui-color-scheme',
+        cssVarPrefix: 'template',
+      },
+      colorSchemes,
+      typography: {
+        fontFamily: 'var(--font-roboto)',
+      },
+      shadows,
+      shape,
+      components: {
+        ...inputsCustomizations,
+        ...dataDisplayCustomizations,
+        ...feedbackCustomizations,
+        ...navigationCustomizations,
+        ...surfacesCustomizations,
+        ...themeComponents,
+      },
+    });
+  }, [themeComponents]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  )
+}
